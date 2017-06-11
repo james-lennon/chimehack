671fc00918d7508ea62c3c600b2818e9e2a8ff64
James Lennon <jameslennon321@gmail.com>
2017-06-10 17:32:11 -0700

    Installed pods

--- mobile/Chimehack-Mobile/Chimehack-Mobile/Controller/SetupController.swift
+++ mobile/Chimehack-Mobile/Chimehack-Mobile/Controller/SetupController.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+class SetupController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        
+    }
+    
+}
---
+++ mobile/Chimehack-Mobile/Pods/Target Support Files/Pods-Chimehack-Mobile/Pods-Chimehack-Mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Chimehack_Mobile : NSObject
+@end
+@implementation PodsDummy_Pods_Chimehack_Mobile
+@end
