1289780d35b8299d2e5a596f4304b770c638916a
James Lennon <jameslennon321@gmail.com>
2017-06-10 20:52:47 -0700

    Added some home page interfaces

--- mobile/Chimehack-Mobile/Chimehack-Mobile/Controller/MainCameraController.swift
+++ mobile/Chimehack-Mobile/Chimehack-Mobile/Controller/MainCameraController.swift
@@ -9,20 +9,69 @@
 import UIKit
 import SnapKit
 
+enum MainViewMode {
+    case camera
+    case words
+    case user
+}
+
 class MainCameraController: UIViewController {
     
     private let cameraButton = CameraButton()
+    private let wordsButton = UIButton()
+    private let userButton = UIButton()
+    
+    private let userController = UserController()
+    private let wordsController = WordsController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         view.backgroundColor = UIColor.white
         
+        /* Setup child view controllers */
+        
+        userController.willMove(toParentViewController: self)
+        wordsController.willMove(toParentViewController: self)
+        addChildViewController(userController)
+        addChildViewController(wordsController)
+        userController.didMove(toParentViewController: self)
+        wordsController.didMove(toParentViewController: self)
+        
         view.addSubview(cameraButton)
         cameraButton.snp.makeConstraints { (make) in
             make.centerX.equalTo(view)
             make.bottom.equalTo(view).offset(-20)
         }
+        
+        view.addSubview(userButton)
+        userButton.setImage(UIImage(named: "user"), for: .normal)
+        userButton.imageView?.contentMode = .scaleAspectFit
+        userButton.addTarget(self, action: #selector(userTapped), for: .touchUpInside)
+        userButton.snp.makeConstraints { (make) in
+            make.width.height.equalTo(40)
+            make.bottom.equalTo(view).offset(-20)
+            make.left.equalTo(view).offset(40)
+        }
+        
+        view.addSubview(wordsButton)
+        wordsButton.setImage(UIImage(named: "words"), for: .normal)
+        wordsButton.imageView?.contentMode = .scaleAspectFit
+        wordsButton.addTarget(self, action: #selector(wordsTapped), for: .touchUpInside)
+        wordsButton.snp.makeConstraints { (make) in
+            make.width.height.equalTo(40)
+            make.bottom.equalTo(view).offset(-20)
+            make.right.equalTo(view).offset(-40)
+        }
+        
+        /* child VCs */
+        view.addSubview(userController.view)
+        userController.view.frame.size = view.frame.size
+        userController.view.frame.origin = CGPoint(x: -view.frame.width, y: 0)
+        
+        view.addSubview(wordsController.view)
+        wordsController.view.frame.size = view.frame.size
+        wordsController.view.frame.origin = CGPoint(x: view.frame.width, y: 0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,6 +82,26 @@ class MainCameraController: UIViewController {
             navVc.navigationBar.isHidden = true
             self.present(navVc, animated: false, completion: nil)
         }
+    }
+    
+    // MARK: - Button Handlers
+    
+    func userTapped() {
+        
+        print(self.userController.view.frame)
+        UIView.animate(withDuration: 0.25) { 
+            self.userController.view.frame.origin = CGPoint.zero
+        }
+        
+    }
+    
+    func wordsTapped() {
+        
+        print(self.wordsController.view.frame)
+        UIView.animate(withDuration: 0.25) {
+            self.wordsController.view.frame.origin = CGPoint.zero
+        }
+        
     }
     
 }
---
+++ mobile/Chimehack-Mobile/Chimehack-Mobile/Controller/UserController.swift
@@ -0,0 +1,20 @@
+//
+//  UserController.swift
+//  Chimehack-Mobile
+//
+//  Created by James Lennon on 6/10/17.
+//  Copyright © 2017 James Lennon. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class UserController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+    }
+}
