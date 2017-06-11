2bfce3d2b01ee4a75845d1f067bab6732cb90581
James Lennon <jameslennon321@gmail.com>
2017-06-10 17:14:06 -0700

    Initial commit

---
+++ mobile/Chimehack-Mobile/Chimehack-Mobile/Model/ImageLookupModel.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLookupModel.swift
+//  Chimehack-Mobile
+//
+//  Created by James Lennon on 6/10/17.
+//  Copyright © 2017 James Lennon. All rights reserved.
+//
+
+import Foundation
---
+++ mobile/Chimehack-Mobile/Chimehack-Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chimehack-Mobile
+//
+//  Created by James Lennon on 6/10/17.
+//  Copyright © 2017 James Lennon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
