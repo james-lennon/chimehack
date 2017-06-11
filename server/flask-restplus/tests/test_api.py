2821a11181ee93716889108faa02935b846e995e
Dan Yu <danyu1996@gmail.com>
2017-06-10 17:23:24 -0700

    adding web server

---
+++ server/flask-restplus/tests/test_api.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from flask import url_for, Blueprint
+
+import flask_restplus as restplus
+
+
+class APITest(object):
+    def test_root_endpoint(self, app):
+        api = restplus.Api(app, version='1.0')
+
+        with app.test_request_context():
+            url = url_for('root')
+            assert url == '/'
+            assert api.base_url == 'http://localhost/'
+
+    def test_root_endpoint_lazy(self, app):
+        api = restplus.Api(version='1.0')
+        api.init_app(app)
+
+        with app.test_request_context():
+            url = url_for('root')
+            assert url == '/'
+            assert api.base_url == 'http://localhost/'
+
+    def test_root_endpoint_with_blueprint(self, app):
+        blueprint = Blueprint('api', __name__, url_prefix='/api')
+        api = restplus.Api(blueprint, version='1.0')
+        app.register_blueprint(blueprint)
+
+        with app.test_request_context():
+            url = url_for('api.root')
+            assert url == '/api/'
+            assert api.base_url == 'http://localhost/api/'
+
+    def test_root_endpoint_with_blueprint_with_subdomain(self, app):
+        blueprint = Blueprint('api', __name__, subdomain='api', url_prefix='/api')
+        api = restplus.Api(blueprint, version='1.0')
+        app.register_blueprint(blueprint)
+
+        with app.test_request_context():
+            url = url_for('api.root')
+            assert url == 'http://api.localhost/api/'
+            assert api.base_url == 'http://api.localhost/api/'
+
+    def test_parser(self):
+        api = restplus.Api()
+        assert isinstance(api.parser(), restplus.reqparse.RequestParser)
+
+    def test_doc_decorator(self, app):
+        api = restplus.Api(app, prefix='/api', version='1.0')
+        params = {'q': {'description': 'some description'}}
+
+        @api.doc(params=params)
+        class TestResource(restplus.Resource):
+            pass
+
+        assert hasattr(TestResource, '__apidoc__')
+        assert TestResource.__apidoc__ == {'params': params}
+
+    def test_doc_with_inheritance(self, app):
+        api = restplus.Api(app, prefix='/api', version='1.0')
+        base_params = {'q': {'description': 'some description', 'type': 'string', 'paramType': 'query'}}
+        child_params = {'q': {'description': 'some new description'}, 'other': {'description': 'another param'}}
+
+        @api.doc(params=base_params)
+        class BaseResource(restplus.Resource):
+            pass
+
+        @api.doc(params=child_params)
+        class TestResource(BaseResource):
+            pass
+
+        assert TestResource.__apidoc__ == {'params': {
+            'q': {
+                'description': 'some new description',
+                'type': 'string',
+                'paramType': 'query'
+            },
+            'other': {'description': 'another param'},
+        }}
+
+    def test_specs_endpoint_not_added(self, app):
+        api = restplus.Api()
+        api.init_app(app, add_specs=False)
+        assert 'specs' not in api.endpoints
+        assert 'specs' not in app.view_functions
+
+    def test_specs_endpoint_not_found_if_not_added(self, app, client):
+        api = restplus.Api()
+        api.init_app(app, add_specs=False)
+        resp = client.get('/swagger.json')
+        assert resp.status_code == 404
+
+    def test_default_endpoint(self, app):
+        api = restplus.Api(app)
+
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('test_resource') == '/test/'
+
+    def test_default_endpoint_lazy(self, app):
+        api = restplus.Api()
+
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        api.init_app(app)
+
+        with app.test_request_context():
+            assert url_for('test_resource') == '/test/'
+
+    def test_default_endpoint_with_blueprint(self, app):
+        blueprint = Blueprint('api', __name__, url_prefix='/api')
+        api = restplus.Api(blueprint)
+        app.register_blueprint(blueprint)
+
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('api.test_resource') == '/api/test/'
+
+    def test_default_endpoint_with_blueprint_with_subdomain(self, app):
+        blueprint = Blueprint('api', __name__, subdomain='api', url_prefix='/api')
+        api = restplus.Api(blueprint)
+        app.register_blueprint(blueprint)
+
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('api.test_resource') == 'http://api.localhost/api/test/'
+
+    def test_default_endpoint_for_namespace(self, app):
+        api = restplus.Api(app)
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('ns_test_resource') == '/ns/test/'
+
+    def test_default_endpoint_lazy_for_namespace(self, app):
+        api = restplus.Api()
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        api.init_app(app)
+
+        with app.test_request_context():
+            assert url_for('ns_test_resource') == '/ns/test/'
+
+    def test_default_endpoint_for_namespace_with_blueprint(self, app):
+        blueprint = Blueprint('api', __name__, url_prefix='/api')
+        api = restplus.Api(blueprint)
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        app.register_blueprint(blueprint)
+
+        with app.test_request_context():
+            assert url_for('api.ns_test_resource') == '/api/ns/test/'
+
+    def test_multiple_default_endpoint(self, app):
+        api = restplus.Api(app)
+
+        @api.route('/test2/')
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('test_resource') == '/test/'
+            assert url_for('test_resource_2') == '/test2/'
+
+    def test_multiple_default_endpoint_lazy(self, app):
+        api = restplus.Api()
+
+        @api.route('/test2/')
+        @api.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        api.init_app(app)
+
+        with app.test_request_context():
+            assert url_for('test_resource') == '/test/'
+            assert url_for('test_resource_2') == '/test2/'
+
+    def test_multiple_default_endpoint_for_namespace(self, app):
+        api = restplus.Api(app)
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test2/')
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        with app.test_request_context():
+            assert url_for('ns_test_resource') == '/ns/test/'
+            assert url_for('ns_test_resource_2') == '/ns/test2/'
+
+    def test_multiple_default_endpoint_lazy_for_namespace(self, app):
+        api = restplus.Api()
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test2/')
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        api.init_app(app)
+
+        with app.test_request_context():
+            assert url_for('ns_test_resource') == '/ns/test/'
+            assert url_for('ns_test_resource_2') == '/ns/test2/'
+
+    def test_multiple_default_endpoint_for_namespace_with_blueprint(self, app):
+        blueprint = Blueprint('api', __name__, url_prefix='/api')
+        api = restplus.Api(blueprint)
+        ns = api.namespace('ns', 'Test namespace')
+
+        @ns.route('/test2/')
+        @ns.route('/test/')
+        class TestResource(restplus.Resource):
+            pass
+
+        app.register_blueprint(blueprint)
+
+        with app.test_request_context():
+            assert url_for('api.ns_test_resource') == '/api/ns/test/'
+            assert url_for('api.ns_test_resource_2') == '/api/ns/test2/'
+
+    def test_ns_path_prefixes(self, app):
+        api = restplus.Api()
+        ns = restplus.Namespace('test_ns', description='Test namespace')
+
+        @ns.route('/test/', endpoint='test_resource')
+        class TestResource(restplus.Resource):
+            pass
+
+        api.add_namespace(ns, '/api_test')
+        api.init_app(app)
+
+        with app.test_request_context():
+            assert url_for('test_resource') == '/api_test/test/'
---
+++ server/flask-restplus/tests/test_marshalling.py
@@ -0,0 +1,328 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pytest
+
+from flask_restplus import (
+    marshal, marshal_with, marshal_with_field, fields, Api, Resource
+)
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+
+
+# Add a dummy Resource to verify that the app is properly set.
+class HelloWorld(Resource):
+    def get(self):
+        return {}
+
+
+class MarshallingTest(object):
+    def test_marshal(self):
+        model = OrderedDict([('foo', fields.Raw)])
+        marshal_dict = OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        output = marshal(marshal_dict, model)
+        assert output == {'foo': 'bar'}
+
+    def test_marshal_with_envelope(self):
+        model = OrderedDict([('foo', fields.Raw)])
+        marshal_dict = OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        output = marshal(marshal_dict, model, envelope='hey')
+        assert output == {'hey': {'foo': 'bar'}}
+
+    def test_marshal_with_skip_none(self):
+        model = OrderedDict([('foo', fields.Raw), ('bat', fields.Raw), ('qux', fields.Raw)])
+        marshal_dict = OrderedDict([('foo', 'bar'), ('bat', None)])
+        output = marshal(marshal_dict, model, skip_none=True)
+        assert output == {'foo': 'bar'}
+
+    def test_marshal_decorator(self):
+        model = OrderedDict([('foo', fields.Raw)])
+
+        @marshal_with(model)
+        def try_me():
+            return OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        assert try_me() == {'foo': 'bar'}
+
+    def test_marshal_decorator_with_envelope(self):
+        model = OrderedDict([('foo', fields.Raw)])
+
+        @marshal_with(model, envelope='hey')
+        def try_me():
+            return OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+
+        assert try_me() == {'hey': {'foo': 'bar'}}
+
+    def test_marshal_decorator_with_skip_none(self):
+        model = OrderedDict([('foo', fields.Raw), ('bat', fields.Raw), ('qux', fields.Raw)])
+
+        @marshal_with(model, skip_none=True)
+        def try_me():
+            return OrderedDict([('foo', 'bar'), ('bat', None)])
+
+        assert try_me() == {'foo': 'bar'}
+
+    def test_marshal_decorator_tuple(self):
+        model = OrderedDict([('foo', fields.Raw)])
+
+        @marshal_with(model)
+        def try_me():
+            headers = {'X-test': 123}
+            return OrderedDict([('foo', 'bar'), ('bat', 'baz')]), 200, headers
+        assert try_me() == ({'foo': 'bar'}, 200, {'X-test': 123})
+
+    def test_marshal_decorator_tuple_with_envelope(self):
+        model = OrderedDict([('foo', fields.Raw)])
+
+        @marshal_with(model, envelope='hey')
+        def try_me():
+            headers = {'X-test': 123}
+            return OrderedDict([('foo', 'bar'), ('bat', 'baz')]), 200, headers
+
+        assert try_me() == ({'hey': {'foo': 'bar'}}, 200, {'X-test': 123})
+
+    def test_marshal_decorator_tuple_with_skip_none(self):
+        model = OrderedDict([('foo', fields.Raw), ('bat', fields.Raw), ('qux', fields.Raw)])
+
+        @marshal_with(model, skip_none=True)
+        def try_me():
+            headers = {'X-test': 123}
+            return OrderedDict([('foo', 'bar'), ('bat', None)]), 200, headers
+
+        assert try_me() == ({'foo': 'bar'}, 200, {'X-test': 123})
+
+    def test_marshal_field_decorator(self):
+        model = fields.Raw
+
+        @marshal_with_field(model)
+        def try_me():
+            return 'foo'
+        assert try_me() == 'foo'
+
+    def test_marshal_field_decorator_tuple(self):
+        model = fields.Raw
+
+        @marshal_with_field(model)
+        def try_me():
+            return 'foo', 200, {'X-test': 123}
+        assert try_me() == ('foo', 200, {'X-test': 123})
+
+    def test_marshal_field(self):
+        model = OrderedDict({'foo': fields.Raw()})
+        marshal_fields = OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        output = marshal(marshal_fields, model)
+        assert output == {'foo': 'bar'}
+
+    def test_marshal_tuple(self):
+        model = OrderedDict({'foo': fields.Raw})
+        marshal_fields = OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        output = marshal((marshal_fields,), model)
+        assert output == [{'foo': 'bar'}]
+
+    def test_marshal_tuple_with_envelope(self):
+        model = OrderedDict({'foo': fields.Raw})
+        marshal_fields = OrderedDict([('foo', 'bar'), ('bat', 'baz')])
+        output = marshal((marshal_fields,), model, envelope='hey')
+        assert output == {'hey': [{'foo': 'bar'}]}
+
+    def test_marshal_tuple_with_skip_none(self):
+        model = OrderedDict([('foo', fields.Raw), ('bat', fields.Raw), ('qux', fields.Raw)])
+        marshal_fields = OrderedDict([('foo', 'bar'), ('bat', None)])
+        output = marshal((marshal_fields,), model, skip_none=True)
+        assert output == [{'foo': 'bar'}]
+
+    def test_marshal_nested(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.Nested({
+                'fye': fields.String,
+            }))
+        ])
+
+        marshal_fields = OrderedDict([
+            ('foo', 'bar'), ('bat', 'baz'), ('fee', {'fye': 'fum'})
+        ])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([
+            ('foo', 'bar'), ('fee', OrderedDict([('fye', 'fum')]))
+        ])
+        assert output == expected
+
+    def test_marshal_nested_with_non_null(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.Nested(
+                OrderedDict([
+                    ('fye', fields.String),
+                    ('blah', fields.String)
+                ]), allow_null=False))
+        ])
+        marshal_fields = [OrderedDict([('foo', 'bar'),
+                                       ('bat', 'baz'),
+                                       ('fee', None)])]
+        output = marshal(marshal_fields, model)
+        expected = [OrderedDict([
+            ('foo', 'bar'),
+            ('fee', OrderedDict([('fye', None), ('blah', None)]))
+        ])]
+        assert output == expected
+
+    def test_marshal_nested_with_null(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.Nested(
+                OrderedDict([
+                    ('fye', fields.String),
+                    ('blah', fields.String)
+                ]), allow_null=True))
+        ])
+        marshal_fields = OrderedDict([('foo', 'bar'),
+                                      ('bat', 'baz'),
+                                      ('fee', None)])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([('foo', 'bar'), ('fee', None)])
+        assert output == expected
+
+    def test_allow_null_presents_data(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.Nested(
+                OrderedDict([
+                    ('fye', fields.String),
+                    ('blah', fields.String)
+                ]), allow_null=True))
+        ])
+        marshal_fields = OrderedDict([('foo', 'bar'),
+                                      ('bat', 'baz'),
+                                      ('fee', {'blah': 'cool'})])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([
+            ('foo', 'bar'),
+            ('fee', OrderedDict([('fye', None), ('blah', 'cool')]))
+        ])
+        assert output == expected
+
+    def test_marshal_nested_property(self):
+        class TestObject(object):
+            @property
+            def fee(self):
+                return {'blah': 'cool'}
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.Nested(
+                OrderedDict([
+                    ('fye', fields.String),
+                    ('blah', fields.String)
+                ]), allow_null=True))
+        ])
+        obj = TestObject()
+        obj.foo = 'bar'
+        obj.bat = 'baz'
+        output = marshal([obj], model)
+        expected = [OrderedDict([
+            ('foo', 'bar'),
+            ('fee', OrderedDict([
+                ('fye', None),
+                ('blah', 'cool')
+            ]))
+        ])]
+        assert output == expected
+
+    def test_marshal_list(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.List(fields.String))
+        ])
+        marshal_fields = OrderedDict([('foo', 'bar'),
+                                      ('bat', 'baz'),
+                                      ('fee', ['fye', 'fum'])])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([('foo', 'bar'), ('fee', (['fye', 'fum']))])
+        assert output == expected
+
+    def test_marshal_list_of_nesteds(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.List(fields.Nested({
+                'fye': fields.String
+            })))
+        ])
+        marshal_fields = OrderedDict([('foo', 'bar'),
+                                      ('bat', 'baz'),
+                                      ('fee', {'fye': 'fum'})])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([('foo', 'bar'),
+                                ('fee', [OrderedDict([('fye', 'fum')])])])
+        assert output == expected
+
+    def test_marshal_list_of_lists(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('fee', fields.List(fields.List(
+                fields.String)))
+        ])
+        marshal_fields = OrderedDict([('foo', 'bar'),
+                                      ('bat', 'baz'),
+                                      ('fee', [['fye'], ['fum']])])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([('foo', 'bar'), ('fee', [['fye'], ['fum']])])
+        assert output == expected
+
+    def test_marshal_nested_dict(self):
+        model = OrderedDict([
+            ('foo', fields.Raw),
+            ('bar', OrderedDict([
+                ('a', fields.Raw),
+                ('b', fields.Raw),
+            ])),
+        ])
+        marshal_fields = OrderedDict([('foo', 'foo-val'),
+                                      ('bar', 'bar-val'),
+                                      ('bat', 'bat-val'),
+                                      ('a', 1), ('b', 2), ('c', 3)])
+        output = marshal(marshal_fields, model)
+        expected = OrderedDict([('foo', 'foo-val'),
+                                ('bar', OrderedDict([('a', 1), ('b', 2)]))])
+        assert output == expected
+
+    @pytest.mark.options(debug=True)
+    def test_will_prettyprint_json_in_debug_mode(self, app, client):
+        api = Api(app)
+
+        class Foo1(Resource):
+            def get(self):
+                return {'foo': 'bar', 'baz': 'asdf'}
+
+        api.add_resource(Foo1, '/foo', endpoint='bar')
+
+        foo = client.get('/foo')
+
+        # Python's dictionaries have random order (as of "new" Pythons,
+        # anyway), so we can't verify the actual output here.  We just
+        # assert that they're properly prettyprinted.
+        lines = foo.data.splitlines()
+        lines = [line.decode() for line in lines]
+        assert "{" == lines[0]
+        assert lines[1].startswith('    ')
+        assert lines[2].startswith('    ')
+        assert "}" == lines[3]
+
+        # Assert our trailing newline.
+        assert foo.data.endswith(b'\n')
+
+    def test_json_float_marshalled(self, app, client):
+        api = Api(app)
+
+        class FooResource(Resource):
+            fields = {'foo': fields.Float}
+
+            def get(self):
+                return marshal({"foo": 3.0}, self.fields)
+
+        api.add_resource(FooResource, '/api')
+
+        resp = client.get('/api')
+        assert resp.status_code == 200
+        assert resp.data.decode('utf-8') == '{"foo": 3.0}\n'
